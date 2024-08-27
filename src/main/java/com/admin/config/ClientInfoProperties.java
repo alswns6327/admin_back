eace986e103310bdb2c938659e5c3212c7d2f8d8
alswns6327 <alswns6327@naver.com>
2024-08-27 12:43:36 +0900

    client 연결 정보 환경 구분

--- src/main/java/com/admin/config/ClientInfoProperties.java
+++ src/main/java/com/admin/config/ClientInfoProperties.java
@@ -2,10 +2,12 @@ package com.admin.config;
 
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
+@Setter
 @Component
 @ConfigurationProperties("client")
 public class ClientInfoProperties {
